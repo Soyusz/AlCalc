1a9e23c2b17781ccee9039aaca658a80e5c70e4e
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-01-06 15:45:55 +0100

    screen templates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,11 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { Header } from "./components/Header";
 import { Navigator } from "./components/Navigation/Navigator";
-import { Sidebar } from "./components/Sidebar";
-import { Fallback404 } from "./views/Fallback404/Fallback404";
-import { Home } from "./views/Home/Home";
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState(false);
-
   return (
-    <Container>
-      <Header onIconClick={() => setShowSidebar(!showSidebar)} />
-      <Sidebar show={showSidebar} />
+    <>
       <Navigator />
-    </Container>
+    </>
   );
 }
-
-const Container = styled.div`
-  height: 100vh;
-  display: grid;
-  grid-template: auto 1fr / auto 100vw;
-  overflow: hidden;
-
-  .Header {
-    grid-column: 1 / 3;
-  }
-`;
 
 export default App;
---
+++ client/src/views/Templates/DefaultTemplate.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export const DefaultTemplate: React.FC = ({ children }) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  return (
+    <Container>
+      <Header onIconClick={() => setShowSidebar(!showSidebar)} />
+      <Sidebar show={showSidebar} />
+      <Content>{children}</Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template: auto 1fr / auto 100vw;
+  overflow: hidden;
+
+  .Header {
+    grid-column: 1 / 3;
+  }
+`;
+
+const Content = styled.div`
+  padding: 20px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
