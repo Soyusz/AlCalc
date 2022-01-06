374ec446ac76f1de83d9a4e9448966786cee5272
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-01-06 13:58:47 +0100

    add sidebar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,18 +2,25 @@ import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { titleMap } from "./Navigation/TitleMap";
 
-export const Header = () => {
-  const params = useLocation();
-  console.log(params);
+type HeaderProps = {
+  onIconClick: () => void;
+};
+
+export const Header = ({ onIconClick }: HeaderProps) => {
   return (
-    <Container>
-      <Title>{titleMap.get(params.pathname)}</Title>
+    <Container className="Header">
+      <SidebarIcon onClick={onIconClick}>E</SidebarIcon>
+      <Title>AlCalc</Title>
     </Container>
   );
 };
 
 const Container = styled.div`
   background-color: #0066ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
 `;
 
 const Title = styled.div`
@@ -22,4 +29,8 @@ const Title = styled.div`
   text-align: center;
   color: white;
   padding: 20px 10px;
+`;
+
+const SidebarIcon = styled.div`
+  font-weight: 900;
 `;
--- client/src/components/Navigation/Navigator.tsx
+++ client/src/components/Navigation/Navigator.tsx
@@ -1,12 +1,9 @@
-import { ReactNode } from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import { Header } from "../Header";
 import { Home } from "../../views/Home/Home";
 import { Fallback404 } from "../../views/Fallback404/Fallback404";
 
 export const Navigator = () => (
   <Router>
-    <Header />
     <Routes>
       <Route path={"/"} element={<Home />} />
       <Route path={"/404"} element={<Fallback404 />} />
