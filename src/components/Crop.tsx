502c8e23b5c9613f9d1c54020d5c776bd6dff4dc
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-05-21 21:25:36 +0200

    cropper remembers settings

--- src/components/Crop.tsx
+++ src/components/Crop.tsx
@@ -1,14 +1,73 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import Cropper from 'react-easy-crop'
-import { Point, Area } from 'react-easy-crop/types'
+import { Point, Area, Size } from 'react-easy-crop/types'
 import styled from 'styled-components'
-import { getCroppedImg } from '../utils/getCroppedImage'
-import { getImageSize } from '../utils/getImageSize'
 
 type Props = {
   image?: string
   onChange: (area: Area) => void
-  onClick: () => void
+  onClick?: () => void
+  initialArea?: Area
+}
+
+export const Crop = (p: Props) => {
+  const [crop, setCrop] = useState<Point | undefined>(p.initialArea || { x: 0, y: 0 })
+  const [zoom, setZoom] = useState<number | undefined>()
+  const containerRef = useRef<null | HTMLDivElement>(null)
+  const [cropSize, setCropSize] = useState<undefined | Size>()
+
+  /* useEffect(() => {
+    if (!p.image || !containerRef.current) return
+    console.log('calc zoom')
+    const containerSize = containerRef.current.clientHeight
+    getImageSize(p.image).then((imageSize) => {
+      const naturalRatio = containerSize / Math.max(imageSize.height, imageSize.width)
+      const minImageSize = Math.min(imageSize.height, imageSize.width)
+      const minRatio = containerSize / (minImageSize * naturalRatio)
+      const ratio = p.initialArea && containerSize / (p.initialArea.width * naturalRatio)
+    })
+    }, [p.image, p.initialArea]) */
+
+  const onCropComplete = useCallback((_: Area, croppedArea: Area) => p.onChange(croppedArea), [p.onChange])
+
+  useEffect(() => {
+    if (!containerRef.current) return
+    const currentSize = {
+      width: containerRef.current.clientWidth,
+      height: containerRef.current.clientHeight,
+    }
+    if (currentSize.height === cropSize?.height && currentSize.width === cropSize?.width) return
+    setCropSize(currentSize)
+  })
+
+  return (
+    <Container ref={containerRef} onClick={p.onClick}>
+      {crop && cropSize && (
+        <Cropper
+          /* values */
+          zoom={zoom}
+          crop={crop}
+          cropSize={cropSize}
+          initialCroppedAreaPixels={p.initialArea}
+          /* handlers */
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+          /* tested */
+          image={p.image}
+          aspect={1}
+          objectFit="contain"
+          style={{
+            containerStyle: { position: 'relative', height: '100%', width: '100%' },
+            cropAreaStyle: {
+              width: '100%',
+              height: '100%',
+            },
+          }}
+        />
+      )}
+    </Container>
+  )
 }
 
 const Container = styled.div`
@@ -18,56 +77,3 @@ const Container = styled.div`
   overflow: hidden;
   border-radius: 10px;
 `
-
-export const Crop = (p: Props) => {
-  const containerRef = useRef<null | HTMLDivElement>(null)
-  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 })
-  const [minZoom, setMinZoom] = useState<number>()
-  const [zoom, setZoom] = useState(1.157)
-
-  const onCropComplete = useCallback((_, croppedArea: Area) => p.onChange(croppedArea), [])
-
-  const getCropSize = () => {
-    if (!containerRef.current) return
-    return {
-      width: containerRef.current.clientWidth,
-      height: containerRef.current.clientHeight,
-    }
-  }
-
-  useEffect(() => {
-    if (!p.image || !containerRef.current) return
-    const containerSize = containerRef.current.clientHeight
-    getImageSize(p.image).then((imageSize) => {
-      const minImageSize = Math.min(imageSize.height, imageSize.width)
-      const naturalRatio = containerSize / Math.max(imageSize.height, imageSize.width)
-      const ratio = containerSize / (minImageSize * naturalRatio)
-      setMinZoom(ratio)
-      setZoom(ratio)
-    })
-  }, [p.image])
-
-  return (
-    <Container ref={containerRef} onClick={p.onClick}>
-      <Cropper
-        image={p.image}
-        crop={crop}
-        zoom={zoom}
-        aspect={1}
-        onCropChange={setCrop}
-        onCropComplete={onCropComplete}
-        onZoomChange={setZoom}
-        cropSize={getCropSize()}
-        minZoom={minZoom}
-        objectFit="contain"
-        style={{
-          containerStyle: { position: 'relative', height: '100%', width: '100%' },
-          cropAreaStyle: {
-            width: '100%',
-            height: '100%',
-          },
-        }}
-      />
-    </Container>
-  )
-}
--- src/views/AddPost/AddPost.tsx
+++ src/views/AddPost/AddPost.tsx
@@ -1,14 +1,81 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { Area } from 'react-easy-crop/types'
 import styled from 'styled-components'
+import { Crop } from '../../components/Crop'
+import { blobToUri } from '../../utils/blobToUri'
+import { getCroppedImg } from '../../utils/getCroppedImage'
+
+type ProcessedImage = {
+  crop?: Area
+  source: string
+  outcome: string
+}
 
 export const AddPost = () => {
-  const [images, setImages] = useState<string[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [images, setImages] = useState<ProcessedImage[]>([])
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>()
+  const currentImage = (currentImageIndex !== undefined && images[currentImageIndex]) || undefined
 
-  return <Container>add post</Container>
+  const handleImageSelected = async () => {
+    const newFile = inputRef.current?.files?.[0]
+    if (!newFile) return
+    const based = await blobToUri(newFile)
+    const newImage: ProcessedImage = {
+      source: based,
+      outcome: based,
+    }
+    setImages([...images, newImage])
+    setCurrentImageIndex(currentImageIndex === undefined ? 0 : currentImageIndex + 1)
+  }
+
+  const handleCropChange = async (newCrop: Area) => {
+    if (!currentImage) return
+    const newImage: ProcessedImage = {
+      ...currentImage,
+      crop: newCrop,
+      outcome: await getCroppedImg(currentImage.source, newCrop),
+    }
+    setImages((prev) => [
+      ...prev.map((image, index) => {
+        if (index === currentImageIndex) return newImage
+        return image
+      }),
+    ])
+  }
+
+  return (
+    <Container>
+      <input type="file" ref={inputRef} onChange={handleImageSelected} hidden />
+      <Crop
+        onChange={handleCropChange}
+        image={currentImage?.source}
+        /* initialArea={{ width: 667, height: 667, x: 557, y: 1085 }} */
+        initialArea={currentImage?.crop}
+        key={currentImageIndex}
+      />
+      <Preview>
+        {images.map((image, index) => (
+          <img src={image.outcome} key={index} onClick={() => setCurrentImageIndex(index)} />
+        ))}
+        <div onClick={() => inputRef.current?.click()}>+</div>
+      </Preview>
+    </Container>
+  )
 }
 
 const Container = styled.div`
   background: ${(props) => props.theme.colors.white};
   padding: ${(props) => props.theme.spacing.s};
   padding-top: 30px;
+`
+
+const Preview = styled.div`
+  display: flex;
+  > img,
+  > div {
+    margin: 10px;
+    height: 50px;
+    width: 50px;
+  }
 `
